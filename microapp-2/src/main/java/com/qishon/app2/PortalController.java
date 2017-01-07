4c4bdb80437a5cb5d07c777e345438ed1b70f1cc
yanping.li <yanping.li@qishon.com>
2017-01-07 15:01:05 +0800

    commit six

---
+++ microapp-2/src/main/java/com/qishon/app2/PortalController.java
@@ -0,0 +1,34 @@
+package com.qishon.app2;
+
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.MetricRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * portal controller
+ *
+ * @author linux_china
+ */
+@RestController
+@RefreshScope
+public class PortalController {
+    @Value("${developer}")
+    private String nick;
+    @Autowired
+    private CounterService counterService;
+    @Autowired
+    private MetricRegistry registry;
+
+    @GetMapping("/")
+    public String welcome() {
+        counterService.increment("counter.page.welcome");
+        Counter evictions = registry.counter("dropwizard.page.welcome");
+        evictions.inc(2);
+        return "hello" + nick;
+    }
+}
